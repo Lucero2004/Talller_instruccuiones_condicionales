477429ebf4a649ca2bc15540bffe4aa4d2b8a32d
luceritopico2020@gmail.com <luceritopico2020@gmail.com>
2023-03-13 22:47:36 -0500

    se agregó Talller_instruccuiones_condicionales

---
+++ ejercicio-8/README.md
@@ -0,0 +1,9 @@
+# ejercicio-8
+# programa que lea 2 numeros enteros y determine si uno es multiplo de otro
+
+# Analisis
+
+# Diseño
+
+### Diagrama de flujo
+![Diagrama de flujo](diagrama.png"Diagrama de flujo")
---
+++ ejercicio-8/ejercicio-8.py
@@ -0,0 +1,12 @@
+#Ejercicio-8
+print("------------------------------------------------------")
+print("programa que lea 2 numeros enteros y determine si uno es multiplo del otro")
+print("------------------------------------------------------")
+a=int(input("Ingerese el primer numero: "))
+b=int(input("Ingrese el segundo numero: "))
+
+if a%b==0:
+    print("El primer numero es multiplo del segundo")
+else:
+    print("El primero no es multiplo del segundo")
+
