f0dc66874077b95922e1d6cc7073cd883ea9bdbf
luceritopico2020@gmail.com <luceritopico2020@gmail.com>
2023-03-13 22:21:54 -0500

    se agregó Talller_instruccuiones_condicionales

---
+++ ejercicio-7/README.md
@@ -0,0 +1,10 @@
+# ejercicio-7
+# Programa para determinar si dados 3 numeros enteros,la suma de los 2 primeros es igual al tercero.
+
+# Analisis
+
+# Diseño
+
+### Diagrama de flujo
+![Diagrama de flujo](diagrama.png"Diagrama de flujo")
+
---
+++ ejercicio-7/ejercicio-7.py
@@ -0,0 +1,11 @@
+# Ejercicio-7
+# Programa para determinar si dados 3 numeros enteros,la suma de los 2 primeros es igual al tercero.
+
+a=int(input("Ingerese el valor del primer numero: "))
+b=int(input("Ingerese el valor del segundo numero: "))
+c=int(input("Ingerese el valor del tercer numero: "))
+
+if c==(a+b):
+    print("La suma de los 2 primeros numeros es igual al ultimo numero digitado")
+else:
+    print("La suma de los 2 primeros numeros no son iguales al ultimo digitado")
