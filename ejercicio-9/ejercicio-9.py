5ad1caebfe24b74a1b1922ddf4172e0da16fb4e9
luceritopico2020@gmail.com <luceritopico2020@gmail.com>
2023-03-13 23:17:46 -0500

    se agregó Talller_instruccuiones_condicionales

--- ejercicio-9/ejercicio-9.py
+++ ejercicio-9/ejercicio-9.py
@@ -1,15 +1,16 @@
 # ejercicio-9
-# Programa para determinar si un triangulo es obtuso,recto o agudo 
-import math
-a=int(input("Ingrese el valor del lado 1: "))
-b=int(input("Ingrese el valor del lado 2: "))
-c=int(input("Ingrese el valor del lado 3: "))
-print("-------------------------------")
+print("--------------------------------------------------------------------")
+print("Programa para determinar si un triangulo es obtuso,recto o agudo ")
+print("-------------------------------------------------------")
 
-if b**2>b**2:
-   if r==b**2:
-     print("Es un triangulo Recto")
-elif r<c**2: 
+a=float(input("Ingrese el valor del lado 1: "))
+b=float(input("Ingrese el valor del lado 2: "))
+c=float(input("Ingrese el valor del lado 3: "))
+print("---------------------------------")
+
+if (c**2)==(a**2)+(b**2):
+   print("Es un triangulo Recto")
+if (c**2)>(a**2)+(b**2):
    print("Es un triangulo obtuso")
-if 
+if (c**2)<(a**2)+(b**2):
    print("El triangulo es agudo ")
